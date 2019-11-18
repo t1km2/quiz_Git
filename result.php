c05852cd59b39ba5ecc2161ed8dfe91000df6d06
Takumi Nisato <g017a1337@gmail.com>
2019-11-18 15:39:42 +0900

    result.php validation add

--- result.php
+++ result.php
@@ -2,8 +2,13 @@
 
 require_once('common.php');
 
-$qid = $_GET['qid'];
+$qid = isset($_GET['qid'])? $_GET['qid']:-1;
 $answer=$_GET['answer'];
+
+if($qid==-1 || !is_numeric($qid) || !((0<=$qid) && ($qid<count($question))) ){
+    echo 'エラー: $qid invalid';
+    exit(1);
+}
 
 if($question[$qid][1]==$answer){
     echo "正解";
