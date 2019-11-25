7f401791226570ae3abba1cc82c2126e735131ae
Takumi Nisato <g017a1337@gmail.com>
2019-11-25 16:23:26 +0900

    question.php textCheck add

--- question.php
+++ question.php
@@ -16,8 +16,22 @@ $i=rand(0,count($question)-1);
 
         <form action="result.php">
             <input type="hidden" name="qid" value="<?= $i ?>">
-            <input type="text" name="answer">
-            <button>回答</button>
+            <input type="text" id ="text-answer" name="answer">
+            <button id="btn-answer">回答</button>
         </form>
+        <script>
+            document.querySelector("#btn-answer").addEventListener("click",(e)=>{
+                let answer = document.querySelector("#text-answer");
+                if(answer.value == ""){
+                    alert("入力してください");
+                    answer.focus();
+                    answer.style.backgroundColor="Pink";
+                    e.preventDefault();
+                }else
+                {
+                    document.querySelector("#btn-answer").innerHTML = "...送信中";
+                }
+            });
+        </script>
     </body>
 </html>
