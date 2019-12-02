344b1587eef7fadee8134413a776bc5accea861e
Takumi Nisato <g017a1337@gmail.com>
2019-12-02 15:10:56 +0900

    result.php change

--- common.php
+++ common.php
@@ -1,10 +1,4 @@
 <?php
-$question=[
-    ["S M T W T F S ","曜日"],
-    ["朝は四本足、昼は二本足、夜は三本足の生き物ってなーーんだ","人間"],
-    ["1+1=?","2"]
-];
-
 function getDB($sql,$param=[]){
     $dsn ='mysql:dbname=quizdb;host=localhost';
     $user='senpai';
--- result.php
+++ result.php
@@ -5,12 +5,16 @@ require_once('common.php');
 $qid = isset($_GET['qid'])? $_GET['qid']:-1;
 $answer=$_GET['answer'];
 
-if($qid==-1 || !is_numeric($qid) || !((0<=$qid) && ($qid<count($question))) ){
+$data=getDB('select max(id) as maxid from Question');
+
+if($qid==-1 || !is_numeric($qid) || !((1<=$qid) && ($qid<=$data['maxid']))){
     echo 'エラー: $qid invalid';
     exit(1);
 }
 
-if($question[$qid][1]==$answer){
+$data=getDB('select question from Question where id=?',[$qid]);
+
+if($data['answer']==$answer){
     echo "正解";
 }else{
     echo "不正解";
